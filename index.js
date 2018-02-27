607ee3c1d4b6420863027599298d25f655515d18
Everett <everett.bowline@gmail.com>
2018-02-27 03:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy (n) {
   const lis = document.querySelectorAll('.ranked-list')
      
     for (let i = 0; i < lis.length; i++) {
-      lis[i].innerHTML = (i + 1).toString()
+      lis[i].innerHTML = parseInt()
     }
 }
 
