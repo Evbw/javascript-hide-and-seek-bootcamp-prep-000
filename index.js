f5f617e826bfc6f97174eafcbee23945a4e151d5
Everett <everett.bowline@gmail.com>
2018-02-27 03:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,28 +18,17 @@ function deepestChild () {
   let current = array
   let next = []
  
-  
   while (current) {
-    // if `current` satisfies the `criteriaFn`, then
-    // return it — recall that `return` will exit the
-    // entire function!
     if (criteriaFn(current)) {
       return current
     }
- 
-    // if `current` is an array, we want to push all of
-    // its elements (which might be arrays) onto `next`
+
     if (Array.isArray(current)) {
       for (let i = 0; i < current.length; i++) {
         next.push(current[i])
       }
     }
- 
-    // after pushing any children (if there
-    // are any) of `current` onto `next`, we want to take
-    // the first element of `next` and make it the
-    // new `current` for the next pass of the `while`
-    // loop
+    
     current = next.shift()
   }
  
